2a1fb8c80175aecf068e1c472d3dc09efa8cf843
Alberto Damo <alberto.damo@gmail.com>
2024-12-09 11:26:29 +0100

    removing dependency, readding the sources

---
+++ lib/raceup_board/components/component.h
@@ -0,0 +1,10 @@
+#ifndef __RACEUP_BOARD_COMPONENT__
+#define __RACEUP_BOARD_COMPONENT__ 
+
+#include <stdint.h>
+
+#include "serial.h"
+#include "gpio.h"
+#include "can.h"
+#include "timer.h"
+#endif // !__RACEUP_BOARD_COMPONENT__
