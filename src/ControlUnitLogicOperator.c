6f6811a56a7b0bb126a305ad399ffeafaf38853b
Alberto Damo <alberto.damo@gmail.com>
2024-12-10 11:13:43 +0100

    little refactoring

--- src/ControlUnitLogicOperator.c
+++ src/ControlUnitLogicOperator.c
@@ -1,10 +1,7 @@
-#include "./ControlUnitLogicOperator.h"
 #include <stdint.h>
-#include <stddef.h>
+#include "./ControlUnitLogicOperator.h"
 
-#include "./cores/core_0/core0.h"
-#include "./cores/core_1/core1.h"
-#include "./cores/core_2/core2.h"
+#include "./cores/cores.h"
 
 void main_cpu_x(uint8_t cpu_num)
 {
