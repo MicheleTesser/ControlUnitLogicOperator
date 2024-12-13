872b9a4a8ee16e6d51022f06a1f476a32ad3930e
Alberto Damo <alberto.damo@gmail.com>
2024-12-13 09:43:04 +0100

    added alive check test for all cores

--- tests/tests/core_1_alive_switch/main.c
+++ tests/tests/core_1_alive_switch/main.c
@@ -1,28 +1,26 @@
 #include "score_lib/test_lib.h"
-#include "ControlUnitLogicOperator.h"
 #include "./src/board_conf/id_conf.h"
 #include "linux_board/linux_board.h"
-#include <stdint.h>
-#include <stdio.h>
+#include "ControlUnitLogicOperator.h"
 #include <sys/cdefs.h>
 #include <threads.h>
-#include <unistd.h>
 
 
-int init_core_0(void* args __attribute_maybe_unused__){
+static int init_core_0(void* args __attribute_maybe_unused__){
     main_cpu_x(0);
     return 0;
 }
 
-int init_core_1(void* args __attribute_maybe_unused__){
+static int init_core_1(void* args __attribute_maybe_unused__){
     main_cpu_x(1);
     return 0;
 }
 
-int init_core_2(void* args __attribute_maybe_unused__){
+static int init_core_2(void* args __attribute_maybe_unused__){
     main_cpu_x(2);
     return 0;
 }
+
 
 int main(void)
 {
@@ -37,22 +35,20 @@ int main(void)
     thrd_create(&core_1, init_core_1, NULL);
     thrd_create(&core_2, init_core_2, NULL);
 
-    uint8_t gpio_val = gpio_read_state(LED_1);
+    uint8_t gpio_val = gpio_read_state(LED_2);
     uint8_t new_val = gpio_val;
 
     while (new_val == gpio_val) {
-        new_val = gpio_read_state(LED_1);
+        new_val = gpio_read_state(LED_2);
     }
-    gpio_val = new_val =gpio_read_state(LED_1);
-    PASSED("gpio switched 1");
+    gpio_val = new_val =gpio_read_state(LED_2);
+    PASSED("core1 alive gpio switched 1");
 
     while (new_val == gpio_val) {
-        new_val = gpio_read_state(LED_1);
+        new_val = gpio_read_state(LED_2);
     }
-    PASSED("gpio switched 2");
+    PASSED("core1 alive gpio switched 2");
 
     print_SCORE();
-
-
     return 0;
 }
---
+++ tests/tests/core_2_alive_switch/main.c
@@ -0,0 +1,55 @@
+#include "score_lib/test_lib.h"
+#include "./src/board_conf/id_conf.h"
+#include "linux_board/linux_board.h"
+#include "ControlUnitLogicOperator.h"
+#include <sys/cdefs.h>
+#include <threads.h>
+
+
+static int init_core_0(void* args __attribute_maybe_unused__){
+    main_cpu_x(0);
+    return 0;
+}
+
+static int init_core_1(void* args __attribute_maybe_unused__){
+    main_cpu_x(1);
+    return 0;
+}
+
+static int init_core_2(void* args __attribute_maybe_unused__){
+    main_cpu_x(2);
+    return 0;
+}
+
+
+int main(void)
+{
+    if(create_virtual_chip() <0){
+        return -1;
+    }
+
+    thrd_t core_0;
+    thrd_t core_1;
+    thrd_t core_2;
+    thrd_create(&core_0, init_core_0, NULL);
+    thrd_create(&core_1, init_core_1, NULL);
+    thrd_create(&core_2, init_core_2, NULL);
+
+
+    uint8_t gpio_val = gpio_read_state(LED_3);
+    uint8_t new_val = gpio_val;
+
+    while (new_val == gpio_val) {
+        new_val = gpio_read_state(LED_3);
+    }
+    gpio_val = new_val =gpio_read_state(LED_3);
+    PASSED("core2 alive gpio switched 1");
+
+    while (new_val == gpio_val) {
+        new_val = gpio_read_state(LED_3);
+    }
+    PASSED("core2 alive gpio switched 2");
+
+    print_SCORE();
+    return 0;
+}
