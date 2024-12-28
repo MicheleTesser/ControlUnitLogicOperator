af56e0e95c599d3b894824275fa9eac730d4fb1d
Alberto Damo <alberto.damo@gmail.com>
2024-12-28 13:49:23 +0100

    added emergency sound duration

--- src/DV/dv.c
+++ src/DV/dv.c
@@ -1,10 +1,15 @@
 #include "dv.h"
 #include "../lib/raceup_board/raceup_board.h"
 #include "../board_conf/gpio/gpio.h"
+#include <stdint.h>
+
+//private
 
 static struct {
     enum AS_STATUS status;
 }DV;
+
+//public
 
 int8_t dv_class_init(void)
 {
@@ -22,6 +27,13 @@ int8_t dv_update_led(void)
 {
     static time_var_microseconds driving_last_time_on =0;
     static time_var_microseconds emergency_last_time_on =0;
+    static time_var_microseconds emergency_sound_last_time_on =0;
+    static uint8_t sound_start = 0;
+
+    if ((timer_time_now() - emergency_sound_last_time_on) > 8 SECONDS) {
+        gpio_set_high(AS_EMERGENCY_SOUND);
+        emergency_sound_last_time_on = timer_time_now();
+    }
 
     switch (DV.status) {
         case AS_OFF:
@@ -36,12 +48,19 @@ int8_t dv_update_led(void)
             gpio_set_high(ASSI_LIGHT_BLU);
             if ((timer_time_now() - driving_last_time_on) > 100 MILLIS ) {
                 gpio_toggle(ASSI_LIGHT_YELLOW);
+                driving_last_time_on = timer_time_now();
             }
             break;
         case AS_EMERGENCY:
             gpio_set_high(ASSI_LIGHT_YELLOW);
             if ((timer_time_now() - emergency_last_time_on) > 100 MILLIS ) {
                 gpio_toggle(ASSI_LIGHT_BLU);
+                emergency_last_time_on = timer_time_now();
+            }
+            if (!sound_start) {
+                sound_start =1;
+                gpio_set_low(AS_EMERGENCY_SOUND);
+                emergency_sound_last_time_on = timer_time_now();
             }
             break;
         case AS_FINISHED:
