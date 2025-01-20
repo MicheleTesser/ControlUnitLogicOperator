c5e0050ace3d3321615afebdb0396206e3557571
Alberto Damo <alberto.damo@gmail.com>
2025-01-20 16:29:53 +0100

    added missing update function in core 1

--- src/cores/core_1/core_1.c
+++ src/cores/core_1/core_1.c
@@ -13,12 +13,13 @@ void main_1(void)
     while (core_alive_blink_init(&alive_blink, CORE_1_ALIVE_BLINK) <0) {}
     while (core_1_feature_init(&feature) <0) {}
 
-    //setup completed
+    //cores sync
     core_status_core_ready(CORE_1);
     while (core_status_ready_state()) {}
 
     //loop
     for(;;){
         core_alive_blink_update(&alive_blink);
+        core_1_feature_update(&feature);
     }
 }
