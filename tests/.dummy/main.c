599e3d2ac65631547f421f079b0ed23077c0cb63
Alberto Damo <alberto.damo@gmail.com>
2025-01-31 08:55:47 +0100

    updated template test main

--- tests/.dummy/main.c
+++ tests/.dummy/main.c
@@ -1,5 +1,4 @@
 #include "score_lib/test_lib.h"
-#include "src/src.h"
 #include "linux_board/linux_board.h"
 #include <stdint.h>
 #include <stdio.h>
@@ -7,39 +6,17 @@
 #include <threads.h>
 #include <unistd.h>
 
-
-int init_core_0(void* args __attribute_maybe_unused__){
-  main_0();
-  return 0;
-}
-
-int init_core_1(void* args __attribute_maybe_unused__){
-  main_1();
-  return 0;
-}
-
-int init_core_2(void* args __attribute_maybe_unused__){
-  main_2();
-  return 0;
-}
-
 int main(void)
 {
-  int8_t err=0;
   if(create_virtual_chip() <0){
-    err--;
     goto end;
   }
 
-  thrd_t core_0;
-  thrd_t core_1;
-  thrd_t core_2;
-  thrd_create(&core_0, init_core_0, NULL);
-  thrd_create(&core_1, init_core_1, NULL);
-  thrd_create(&core_2, init_core_2, NULL);
-
+  if (virtual_can_manager_init()<0) {
+    goto end;
+  }
 
 end:
   print_SCORE();
-  return err;
+  return 0;
 }
