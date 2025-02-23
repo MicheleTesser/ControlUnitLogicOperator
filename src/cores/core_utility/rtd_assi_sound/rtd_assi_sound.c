3f8383e7b908b98b6f709e9915b62bd9d43a0e2e
Alberto Damo <alberto.damo@gmail.com>
2025-02-23 17:00:50 +0100

    Improved the rtd_assi_sound class.

--- src/cores/core_utility/rtd_assi_sound/rtd_assi_sound.c
+++ src/cores/core_utility/rtd_assi_sound/rtd_assi_sound.c
@@ -6,10 +6,14 @@
 
 struct{
   Gpio_h o_rtd_assi_sound;
+  atomic_uchar on_req;
   atomic_bool init_done;
 }GPIO_RTD_ASSI;
 
 struct RtdAssiSound_t{
+  uint8_t desidered_state:1;
+  uint8_t req_change:1;
+  uint8_t user_id:6;
   struct Gpio_h* p_gpio_rtd_assi_sound;
 };
 
@@ -32,7 +36,7 @@ rtd_assi_sound_init(RtdAssiSound_h* const restrict self)
   union RtdAssiSound_h_t_conv conv = {self};
   struct RtdAssiSound_t* const p_self = conv.clear;
 
-  if (!atomic_load(&GPIO_RTD_ASSI.init_done))
+  if (!atomic_exchange(&GPIO_RTD_ASSI.init_done,1))
   {
     hardware_init_gpio(&GPIO_RTD_ASSI.o_rtd_assi_sound, GPIO_RTD_ASSI_SOUND);
   }
@@ -50,7 +54,14 @@ rtd_assi_sound_start(RtdAssiSound_h* const restrict self)
   union RtdAssiSound_h_t_conv conv = {self};
   struct RtdAssiSound_t* const p_self = conv.clear;
 
-  gpio_set_low(p_self->p_gpio_rtd_assi_sound);
+  p_self->desidered_state = 1;
+  if (!p_self->req_change)
+  {
+    p_self->req_change=1;
+    atomic_fetch_add(&GPIO_RTD_ASSI.on_req, 1);
+    gpio_set_low(p_self->p_gpio_rtd_assi_sound);
+  }
+
 
   return 0;
 }
@@ -61,11 +72,16 @@ rtd_assi_sound_stop(RtdAssiSound_h* const restrict self)
   union RtdAssiSound_h_t_conv conv = {self};
   struct RtdAssiSound_t* const p_self = conv.clear;
 
-  if (!GPIO_RTD_ASSI.init_done) {
-  
+  p_self->desidered_state = 0;
+  if(p_self->req_change &&
+      p_self->desidered_state != !!atomic_load(&GPIO_RTD_ASSI.on_req) &&
+      atomic_load(&GPIO_RTD_ASSI.on_req) == 1)
+  {
+    p_self->req_change=0;
+    atomic_fetch_sub(&GPIO_RTD_ASSI.on_req, 1);
+    gpio_set_high(p_self->p_gpio_rtd_assi_sound);
   }
 
-  gpio_set_high(p_self->p_gpio_rtd_assi_sound);
 
   return 0;
 }
--- src/cores/core_utility/rtd_assi_sound/rtd_assi_sound.h
+++ src/cores/core_utility/rtd_assi_sound/rtd_assi_sound.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 typedef struct __attribute__((aligned(8))){
-  const uint8_t private_data[8];
+  const uint8_t private_data[16];
 }RtdAssiSound_h;
 
 int8_t
