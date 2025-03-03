88fc37b6d067c1f0c98694cfff281d6a71874b8c
Alberto Damo <alberto.damo@gmail.com>
2025-03-03 11:04:07 +0100

    Implemented and tested the new json_builder library. Adapted the telemetry library and the rest of the code to this change.

--- tests/tests/json_telemetry/main.c
+++ tests/tests/json_telemetry/main.c
@@ -2,7 +2,6 @@
 #include "src/cores/core_1/feature/log/telemetry/telemetry.h"
 #include "linux_board/linux_board.h"
 #include <stdint.h>
-#include <stdio.h>
 #include <sys/cdefs.h>
 #include <threads.h>
 #include <unistd.h>
@@ -12,8 +11,8 @@ float log_var_b = 12.5f;
 unsigned long log_var_c = 912;
 uint8_t log_var_d = 5;
 
-#define LOG_VAR(p_self, VAR, data_type, data_min, data_max, POS)\
-  log_telemetry_add_entry(p_self, #VAR, &log_var_a, data_type, data_min, data_max,POS)
+#define LOG_VAR(p_self, VAR, data_type, POS)\
+  log_telemetry_add_entry(p_self, #VAR, &log_var_a, data_type, POS)
 
 int main(void)
 {
@@ -27,28 +26,28 @@ int main(void)
     goto end;
   }
 
-  if (LOG_VAR(&o_telemetry, log_var_a, DATA_SIGNED,'a', 'c',1)<0)
+  if (LOG_VAR(&o_telemetry, log_var_a, __u8__,1)<0)
   {
     FAILED("failed to store the log entry for log_var_a");
     goto destroy_log;
   }
   PASSED("inserted log_var_a");
 
-  if (LOG_VAR(&o_telemetry, log_var_b, DATA_FLOATED,10, 20, 2)<0)
+  if (LOG_VAR(&o_telemetry, log_var_b, __float__, 2)<0)
   {
     FAILED("failed to store the log entry for log_var_b");
     goto destroy_log;
   }
   PASSED("inserted log_var_b");
 
-  if (LOG_VAR(&o_telemetry, log_var_c, DATA_UNSIGNED, 500, 1000, 3)<0)
+  if (LOG_VAR(&o_telemetry, log_var_c, __u32__, 3)<0)
   {
     FAILED("failed to store the log entry for log_var_c");
     goto destroy_log;
   }
   PASSED("inserted log_var_c");
 
-  if (LOG_VAR(&o_telemetry, log_var_d, DATA_UNSIGNED,0, 20, 3)<0)
+  if (LOG_VAR(&o_telemetry, log_var_d, __u8__, 3)<0)
   {
     PASSED("prevent to store the log entry for log_var_d with already used position");
   }
@@ -57,7 +56,7 @@ int main(void)
     FAILED("did not prevent to store the log entry for log_var_d with already used position");
   }
 
-  if (LOG_VAR(&o_telemetry, log_var_d, DATA_UNSIGNED,0 , 20, 4)<0)
+  if (LOG_VAR(&o_telemetry, log_var_d, __u8__, 4)<0)
   {
     FAILED("failed to store the log entry for log_var_d");
     goto destroy_log;
@@ -68,7 +67,7 @@ int main(void)
 
   PASSED("all the variable has been stored in the log class");
 
-  log_telemetry_lock_json(&o_telemetry);
+  log_telemetry_send(&o_telemetry);
 
 destroy_log:
   log_telemetry_destroy(&o_telemetry);
