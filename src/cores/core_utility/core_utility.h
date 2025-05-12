e843baba9fbcc2f844763708b2ec956c125144b5
Alberto Damo <alberto.damo@gmail.com>
2025-05-12 14:37:22 +0200

    refactor(core_utility.h): Added inclusions to all submodule of core_utility

--- src/cores/core_utility/core_utility.h
+++ src/cores/core_utility/core_utility.h
@@ -2,8 +2,13 @@
 #define __CORE_UTILITY__
 
 #include "alive_blink/alive_blink.h"
+#include "as_node/as_node.h"
+#include "car_speed/car_speed.h"
 #include "core_status/core_status.h"
+#include "driver_input_reader/driver_input_reader.h"
 #include "emergency_module/emergency_module.h"
+#include "imu/imu.h"
+#include "rtd_assi_sound/rtd_assi_sound.h"
 #include "running_status/running_status.h"
 
 #endif // !__CORE_UTILITY__
