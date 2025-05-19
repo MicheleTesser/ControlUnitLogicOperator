75fb62b7b6e4032256423f9f79352b0c5835557a
Alberto Damo <alberto.damo@gmail.com>
2025-05-19 11:19:35 +0200

    feat(Linux serial): Added missing implementation of serial driver
    Added:
      - serial_write_raw();
      - serial_write_int8_t();
      - serial_write_int16_t();
      - serial_write_int32_t();
      - serial_write_uint8_t();
      - serial_write_uint16_t();
      - serial_write_uint32_t();
      - serial_write_float();

--- tests/linux_board/serial/serial.c
+++ tests/linux_board/serial/serial.c
@@ -28,3 +28,26 @@ int8_t serial_write_str(const char* const restrict buffer)
     printf("%s\n",buffer);
     return 0;
 }
+
+int8_t serial_write_raw(const char* const restrict buffer)
+{
+    printf("%s",buffer);
+    return 0;
+}
+
+#define TEMPLATE_SERIAL_WRITE_DIG(TYPE,FORM)\
+int8_t serial_write_##TYPE (const TYPE dig)\
+{\
+    printf(FORM,dig);\
+    return 0;\
+}
+
+TEMPLATE_SERIAL_WRITE_DIG(int8_t, "%d")
+TEMPLATE_SERIAL_WRITE_DIG(int16_t, "%d")
+TEMPLATE_SERIAL_WRITE_DIG(int32_t, "%d")
+
+TEMPLATE_SERIAL_WRITE_DIG(uint8_t, "%d")
+TEMPLATE_SERIAL_WRITE_DIG(uint16_t, "%d")
+TEMPLATE_SERIAL_WRITE_DIG(uint32_t, "%d")
+
+TEMPLATE_SERIAL_WRITE_DIG(float, "%f")
