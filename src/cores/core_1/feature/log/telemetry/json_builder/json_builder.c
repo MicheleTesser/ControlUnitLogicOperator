21c6f0da9ace547a91e6d6fa86b57c72f1cb0146
Alberto Damo <alberto.damo@gmail.com>
2025-05-31 15:14:11 +0200

    fix(json_builder): better length computation

--- src/cores/core_1/feature/log/telemetry/json_builder/json_builder.c
+++ src/cores/core_1/feature/log/telemetry/json_builder/json_builder.c
@@ -127,7 +127,7 @@ uint16_t json_len(const Json_h* const restrict self)
   const union Json_h_t_conv_const conv = {self};
   const struct Json_t* const p_self = conv.clear;
 
-  return p_self->str_json_size;
+  return (uint16_t) strlen(p_self->p_str_json);
 }
 
 int8_t json_destroy(Json_h* self)
