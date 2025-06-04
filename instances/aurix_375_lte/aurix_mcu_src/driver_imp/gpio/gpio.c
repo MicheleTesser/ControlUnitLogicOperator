807d3eec09115c375b0442b55ba3074847db309f
Alberto Damo <alberto.damo@gmail.com>
2025-06-04 11:55:12 +0200

    fix(gpio): fixed gpio impl

--- instances/aurix_375_lte/aurix_mcu_src/driver_imp/gpio/gpio.c
+++ instances/aurix_375_lte/aurix_mcu_src/driver_imp/gpio/gpio.c
@@ -15,8 +15,7 @@
 //gpio
 
 struct GpioRead_t{
-  Ifx_P *port;
-  uint8_t pin_index;
+  enum GPIO_PIN pin_gpio;
 };
 
 #define GPIO_OUTPUT_MODE ((IfxPort_Mode)(IfxPort_OutputMode_pushPull | IfxPort_OutputIdx_general))
@@ -28,13 +27,13 @@ static struct GpioSpec{
   IfxPort_Mode mode;
 }BOARD_GPIOS[__NUM_OF_GPIOS__] =
 {
-  {&MODULE_P33, 0, GPIO_OUTPUT_MODE }, //INFO: GPIO_CORE_0_ALIVE_BLINK, default led 0
-  {&MODULE_P33, 4, GPIO_OUTPUT_MODE }, //INFO: GPIO_CORE_1_ALIVE_BLINK, debug led 2 1
-  {&MODULE_P33, 6, GPIO_OUTPUT_MODE }, //INFO: GPIO_CORE_2_ALIVE_BLINK, debug led 3 2
-  {&MODULE_P23, 1, GPIO_INPUT_MODE }, //TODO: GPIO_RTD_BUTTON, DRIVE_BUTTON 3
-  {&MODULE_P33, 5, GPIO_OUTPUT_MODE }, //INFO: GPIO_RTD_ASSI_SOUND, RTDS 4
-  {&MODULE_P32, 4, GPIO_INPUT_MODE }, //INFO: GPIO_AIR_PRECHARGE_INIT, AIR_1 5
-  {&MODULE_P23, 0, GPIO_INPUT_MODE }, //INFO: GPIO_AIR_PRECHARGE_DONE, AIR_2 6
+  {&MODULE_P33, 0, GPIO_OUTPUT_MODE}, //INFO: GPIO_CORE_0_ALIVE_BLINK, default led 0
+  {&MODULE_P33, 4, GPIO_OUTPUT_MODE}, //INFO: GPIO_CORE_1_ALIVE_BLINK, debug led 2 1
+  {&MODULE_P33, 6, GPIO_OUTPUT_MODE}, //INFO: GPIO_CORE_2_ALIVE_BLINK, debug led 3 2
+  {&MODULE_P23, 1, GPIO_INPUT_MODE}, //TODO: GPIO_RTD_BUTTON, DRIVE_BUTTON 3
+  {&MODULE_P33, 5, GPIO_OUTPUT_MODE}, //INFO: GPIO_RTD_ASSI_SOUND, RTDS 4
+  {&MODULE_P32, 4, GPIO_INPUT_MODE}, //INFO: GPIO_AIR_PRECHARGE_INIT, AIR_1 5
+  {&MODULE_P23, 0, GPIO_INPUT_MODE}, //INFO: GPIO_AIR_PRECHARGE_DONE, AIR_2 6
   {&MODULE_P33, 3, GPIO_OUTPUT_MODE}, //INFO: GPIO_SCS, SCS_SDC_RELAY 7
   {&MODULE_P33, 2, GPIO_OUTPUT_MODE}, //INFO: GPIO_AS_NODE, AS_SDC_RELAY 8
   {&MODULE_P33, 1, GPIO_OUTPUT_MODE}, //INFO: GPIO_RTD_BUTTON_LED, RTD 9
@@ -59,6 +58,15 @@ union Gpio_h_t_conv{
   Gpio_h* const restrict hidden;
   struct Gpio_t* const restrict clear;
 };
+#define DEBUG
+#ifdef DEBUG
+char __assert_gpio_size[sizeof(GpioRead_h)==sizeof(struct GpioRead_t)?+1:-1];
+char __assert_gpio_align[_Alignof(GpioRead_h)==_Alignof(struct GpioRead_t)?+1:-1];
+
+char __assert_gpio_size[sizeof(Gpio_h)==sizeof(struct Gpio_t)?+1:-1];
+char __assert_gpio_align[_Alignof(Gpio_h)==_Alignof(struct Gpio_t)?+1:-1];
+
+#endif /* ifdef DEBUG */
 
 //public
 
@@ -75,14 +83,9 @@ int8_t hardware_init_gpio(Gpio_h* const restrict self ,const enum GPIO_PIN id)
 
   p_info_gpio = &BOARD_GPIOS[id];
 
-  p_self->gpio_read_permission.port = p_info_gpio->port;
-  p_self->gpio_read_permission.pin_index = p_info_gpio->pin_index;
+  p_self->gpio_read_permission.pin_gpio = id;
 
   IfxPort_setPinMode(p_info_gpio->port,p_info_gpio->pin_index,p_info_gpio->mode);
-  if (p_info_gpio->mode == GPIO_OUTPUT_MODE)
-  {
-    gpio_set_high(self);
-  }
 
   return 0;
 }
@@ -94,8 +97,8 @@ int8_t hardware_init_read_permission_gpio(GpioRead_h* const restrict self, const
   struct GpioSpec* p_info_gpio = NULL;
   p_info_gpio = &BOARD_GPIOS[id];
 
-  p_self->port = p_info_gpio->port;
-  p_self->pin_index = p_info_gpio->pin_index;
+  p_self->pin_gpio= id;
+  IfxPort_setPinMode(p_info_gpio->port,p_info_gpio->pin_index,p_info_gpio->mode);
 
   return 0;
 }
@@ -104,27 +107,29 @@ int8_t gpio_toggle(Gpio_h* const restrict self)
 {
   union Gpio_h_t_conv conv = {self};
   struct Gpio_t* p_self = conv.clear;
-  struct GpioSpec* p_info_gpio = p_info_gpio = &BOARD_GPIOS[GPIO_CORE_0_ALIVE_BLINK];
+  struct GpioSpec* p_info_gpio = &BOARD_GPIOS[p_self->gpio_read_permission.pin_gpio];
 
   IfxPort_togglePin(
-      p_self->gpio_read_permission.port,
-      p_self->gpio_read_permission.pin_index);
+      p_info_gpio->port,
+      p_info_gpio->pin_index);
   return 0;
 }
 int8_t gpio_read_state(const GpioRead_h* const restrict self)
 {
   const union GpioRead_h_t_conv_const conv = {self};
   const struct GpioRead_t* p_self = conv.clear;
-  return IfxPort_getPinState(p_self->port,p_self->pin_index);
+  struct GpioSpec* p_info_gpio = &BOARD_GPIOS[p_self->pin_gpio];
+
+  return IfxPort_getPinState(p_info_gpio->port,p_info_gpio->pin_index);
 }
 
 int8_t gpio_set_high(Gpio_h* const restrict self)
 {
   union Gpio_h_t_conv conv = {self};
   struct Gpio_t* p_self = conv.clear;
-  IfxPort_setPinHigh(
-      p_self->gpio_read_permission.port,
-      p_self->gpio_read_permission.pin_index);
+  struct GpioSpec* p_info_gpio = &BOARD_GPIOS[p_self->gpio_read_permission.pin_gpio];
+
+  IfxPort_setPinHigh(p_info_gpio->port,p_info_gpio->pin_index);
   return 0;
 }
 
@@ -132,9 +137,10 @@ int8_t gpio_set_low(Gpio_h* const restrict self)
 {
   union Gpio_h_t_conv conv = {self};
   struct Gpio_t* p_self = conv.clear;
-  IfxPort_setPinLow(
-      p_self->gpio_read_permission.port,
-      p_self->gpio_read_permission.pin_index);
+  struct GpioSpec* p_info_gpio = &BOARD_GPIOS[p_self->gpio_read_permission.pin_gpio];
+
+  IfxPort_setPinLow(p_info_gpio->port,p_info_gpio->pin_index);
+
   return 0;
 }
 
